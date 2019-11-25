b00155059077a6be31203d044731fcf06d9dcdd0
Joaquim <joaquimmy@yahoo.com>
2019-11-25 16:24:45 -0300

    Fix test

--- __tests__/urls.js
+++ __tests__/urls.js
@@ -1,7 +1,6 @@
 const gifs = require('../dist/chop-gifs');
 
 test('should generate valid urls', () => {
-  const regex = /https:\/\/cdn.chop.coffee\/[\d]{1,3}\.gif/;
   [
     'blush',
     'coffee',
@@ -17,12 +16,47 @@ test('should generate valid urls', () => {
     'lick',
     'pout',
     'punch',
+    'slap',
     'smug',
     'stare',
   ].forEach(type => {
+    const regex = new RegExp(`https:\/\/cdn.chop.coffee\/${type}\/[0-9]{1,3}\.gif`);
+
+    console.log('Testing', type);
     const url = gifs[type]();
     for (let i = 0; i < 200; i++) {
-      if (regex.test(url)) {
+      if (!regex.test(url)) {
+        throw new Error(`Url ${url} is not valid!`);
+      }
+    }
+  });
+});
+
+test('regex should not accept invalid urls', () => {
+  [
+    'blush',
+    'coffee',
+    'confused',
+    'cry',
+    'cuddle',
+    'cute',
+    'highfive',
+    'holdhands',
+    'hug',
+    'kiss',
+    'laugh',
+    'lick',
+    'pout',
+    'punch',
+    'slap',
+    'smug',
+    'stare',
+  ].forEach(type => {
+    const regex = new RegExp(`https:\/\/cdn.chop.coffee\/${type}\/[0-9]{1,3}\.gif`);
+
+    const url = gifs[type]();
+    for (let i = 0; i < 200; i++) {
+      if (regex.test('https://cdn.chop/coffee/BACON.gif')) {
         throw new Error(`Url ${url} is not valid!`);
       }
     }
